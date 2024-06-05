6aef69dacaa3b646f279b510c07d886ad67ef95b
mastery3 <94207545+mastery3@users.noreply.github.com>
2024-06-05 11:43:39 -0500

    Fix Issue #1
    Items now drop again when using the enchantment

--- src/main/java/mastery3/unbreakable/mixin/ItemStackMixin.java
+++ src/main/java/mastery3/unbreakable/mixin/ItemStackMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.At;
 import mastery3.unbreakable.Entry;
-
+import net.minecraft.component.DataComponentTypes;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.item.ItemStack;
@@ -18,10 +18,10 @@ import net.minecraft.server.network.ServerPlayerEntity;
 
 @Mixin(ItemStack.class)
 public class ItemStackMixin {
-    @Inject(method="damage", at=@At("HEAD"), cancellable=true)
-    private void inject(int x, net.minecraft.util.math.random.Random random, ServerPlayerEntity entity, Runnable runnable, CallbackInfo cir) {
+    @Inject(method="isDamageable", at=@At("HEAD"), cancellable=true)
+    private void inject(CallbackInfoReturnable<Boolean> cir) {
         if (EnchantmentHelper.getLevel(Entry.UNBREAKABLE, ((ItemStack)(Object)this)) > 0) {
-            ((CallbackInfoReturnable<Boolean>)cir).setReturnValue(Boolean.valueOf(false));
+            cir.setReturnValue(Boolean.valueOf(false));
         }
     }
 }
